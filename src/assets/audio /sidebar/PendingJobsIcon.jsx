d00be07ee1e898ac58bb8674274b5c4f55f55e31
caao1981 <103457346+caao1981@users.noreply.github.com>
2024-02-20 19:01:13 +0300

    Create PendingJobsIcon.jsx

---
+++ src/assets/audio /sidebar/PendingJobsIcon.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+const PendingJobsIcon = () => {
+  return (
+    <svg
+      width="38"
+      height="33"
+      viewBox="0 0 38 33"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M32.9925 31.2247H5.5C3.575 31.2247 2 29.6497 2 27.7247V8.49219C2 6.56719 3.575 4.99219 5.5 4.99219H32.9925C34.9175 4.99219 36.4925 6.56719 36.4925 8.49219V27.7247C36.4925 29.6497 34.935 31.2247 32.9925 31.2247Z"
+        stroke="white"
+        stroke-width="2.4"
+        stroke-miterlimit="10"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M2 11.625H36.4925"
+        stroke="white"
+        stroke-width="2.4"
+        stroke-miterlimit="10"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M10.9956 7.7575V2"
+        stroke="white"
+        stroke-width="2.4"
+        stroke-miterlimit="10"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M27.498 7.7575V2"
+        stroke="white"
+        stroke-width="2.4"
+        stroke-miterlimit="10"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default PendingJobsIcon;
