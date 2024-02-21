e42b9757052ec9aed1ca18f17cd2f0e474f873a7
caao1981 <103457346+caao1981@users.noreply.github.com>
2024-02-21 09:24:51 +0300

    Delete src/assets/audio  directory

--- src/assets/audio /sidebar/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Home } from "./HomeIcon";
-export { default as ActiveJobs } from "./ActiveJobsIcon";
-export { default as PendingJobs } from "./PendingJobsIcon";
-export { default as Profile } from "./ProfileIcon";
-export { default as Chat } from "./ChatIcon";
