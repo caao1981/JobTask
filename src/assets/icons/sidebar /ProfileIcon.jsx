bee7acf55c91fdc230da73323a89276a16936147
caao1981 <103457346+caao1981@users.noreply.github.com>
2024-02-21 11:16:50 +0300

    Create ProfileIcon.jsx

---
+++ src/assets/icons/sidebar /ProfileIcon.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const ProfileIcon = () => {
+  return (
+    <svg
+      width="30"
+      height="28"
+      viewBox="0 0 30 28"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M14.9011 14.0739C18.3748 14.0739 21.1908 11.371 21.1908 8.03694C21.1908 4.70283 18.3748 2 14.9011 2C11.4273 2 8.61133 4.70283 8.61133 8.03694C8.61133 11.371 11.4273 14.0739 14.9011 14.0739Z"
+        stroke="white"
+        stroke-width="2.4"
+        stroke-miterlimit="10"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M27.8379 25.9418C25.783 21.0196 20.7798 17.5381 14.919 17.5381C9.05807 17.5381 4.03701 21.0196 2 25.9418"
+        stroke="white"
+        stroke-width="2.4"
+        stroke-miterlimit="10"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default ProfileIcon;
