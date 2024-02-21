e7864734c8db3f09d34ddedad1adc9f8529912f3
caao1981 <103457346+caao1981@users.noreply.github.com>
2024-02-21 21:20:40 +0300

    Delete src/components/Base/AvatarComponent  directory

--- src/components/Base/AvatarComponent /index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Avatar from '@mui/material/Avatar'
-
-const AvatarComponent = ({ alt, src, ...otherProps }) => {
-  return <Avatar alt={alt} src={src} {...otherProps} />
-}
-
-export default AvatarComponent
