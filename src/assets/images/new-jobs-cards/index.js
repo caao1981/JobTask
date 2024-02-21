d45a5f46dd3f7ac102fdde0067fe105a28fe8dc0
caao1981 <103457346+caao1981@users.noreply.github.com>
2024-02-21 11:35:18 +0300

    Create index.js

---
+++ src/assets/images/new-jobs-cards/index.js
@@ -0,0 +1,10 @@
+export { default as CarTransport } from "";
+export { default as Cleaning } from "./cleaning.png";
+export { default as Deliveries } from "./deliveries.png";
+export { default as FurnitureAssembly } from "./furniture-assembly.png";
+export { default as ManAndVan } from "./man-and-van.png";
+export { default as MobileBarbers } from "./mobile-barbers.png";
+export { default as MobileHairDressers } from "./mobile-hair-dressers.png";
+export { default as MobileNailTechnicians } from "./mobile-nail-technicians.png";
+export { default as Removals } from "./removals.png";
+export { default as ShopAndDeliver } from "./shop-and-deliver.png";
