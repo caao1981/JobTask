fa72b76b4009ad17fec84ca776ce48fcf270f7dc
caao1981 <103457346+caao1981@users.noreply.github.com>
2024-02-22 20:35:55 +0300

    Create index.js

---
+++ src/hooks /index.js
@@ -0,0 +1,8 @@
+export { default as useCustomColors } from "./useCustomColors";
+export { default as useNotification } from "./useNotification";
+export { default as useScrollTop } from "./useScrollTop";
+export { default as useService } from "./useService";
+export { default as useSearch } from "./useSearch";
+export { default as useScrollBottom } from "./useScrollBottom";
+export { default as useGetJobsByStatus } from "./useGetJobsByStatus";
+export { default as useResponsive } from "./useResponsive";
