d67f1b4d3394365d0f4f665f901567bb1d04ecb0
caao1981 <103457346+caao1981@users.noreply.github.com>
2024-02-22 21:02:14 +0300

    Create index.js

---
+++ src/pages/home /index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import HomeComponent from "../../components/UseCase/HomeComponent/HomeComponent";
+
+const Home = () => {
+  return <HomeComponent />;
+};
+
+export default Home;
