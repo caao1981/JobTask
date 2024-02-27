f525208cd0d4b07b21e73838495761b2bce1cef1
caao1981 <103457346+caao1981@users.noreply.github.com>
2024-02-27 11:15:47 +0300

    Delete src/assets/images/home-cards  directory
    Signed-off-by: caao1981 <103457346+caao1981@users.noreply.github.com>

--- src/assets/images/home-cards /index.js
+++
@@ -1,5 +0,0 @@
-export { default as PendingJobs } from "./clock 1.png";
-export { default as ActiveJobs } from "./001-mop.png";
-export { default as CompletedWork } from "./003-mattress.png";
-export { default as MyServices } from "./002-cleaner.png";
-export { default as Profile } from "./004-user-profile.png";
